7d1e4c838d82ade57ca7065e2df20d019f410f36
S.S.Rathod <sawansinghrathod027@gmail.com>
2025-04-29 10:23:50 +0530

    3D Animated Project

---
+++ Basic JS Practices/day1_practice.js
@@ -0,0 +1,33 @@
+let name = "John Doe";
+let age = 20;
+let isStudent = true;
+let hobbies = ["reading", "gaming", "coding"];
+let city;
+let student = {
+    name: "Aman Mishra",
+    age: 21,
+    isStudent: true,
+    hobbies: ["reading books", "playing games", "coding in JS"],
+    city: undefined
+}
+console.log(name); // Output: John Doe
+
+console.log(age); // Output: 20 
+
+console.log(isStudent); // Output: true
+
+console.log(hobbies); // Output: ["reading", "gaming", "coding"]
+
+console.log(city); // Output: undefined
+
+console.log(student); // Output: {name: "Aman Mishra", age: 21, isStudent: true, hobbies: Array(3), city: undefined}
+
+console.log(student.name); // Output: Aman Mishra
+
+console.log(student.age); // Output: 21
+
+console.log(student.isStudent); // Output: true
+
+console.log(student.hobbies); // Output: ["reading books", "playing games", "coding in JS"]
+
+console.log(student.city); // Output: undefined
